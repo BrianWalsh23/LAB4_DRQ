8634876ec7275b58118320cf90d764bb63ac320c
BrianWalsh23 <G00424410@atu.ie.com>
2024-10-04 09:26:59 +0000

    Updated NavBar Comments

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,15 +2,20 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-
+// Exercise 5 Added Navigation Bar
 const NavigationBar = () => {
   return (
+        	/*Background dark and adds dark theme */
         <Navbar bg="dark" data-bs-theme="dark">
           <Container>
+            {/* Creates NavBar */}
             <Navbar.Brand href="/">Navbar</Navbar.Brand>
             <Nav className="me-auto">
+                {/* Link to HomePage */}
               <Nav.Link href="/">Home</Nav.Link>
+              {/* Link to CreatePage */}
               <Nav.Link href="/create">Create</Nav.Link>
+              {/* Link to ReadPage */}
               <Nav.Link href="/read">Read</Nav.Link>
             </Nav>
           </Container>
