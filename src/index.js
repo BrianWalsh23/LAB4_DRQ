60b99cca2432cf2d191f623abe5fcbd9ebe966cc
BrianWalsh23 <G00424410@atu.ie.com>
2024-10-04 09:09:35 +0000

    Exercise 4 Installed bootstrap and imported it into the index.js file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Exercise 4 installed bootstrap and imported to index.js
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
