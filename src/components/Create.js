53f9cc73c1b96b72ecf4330808772ee8299844bd
Brian Walsh <G00424410@atu.ie>
2024-10-18 09:15:35 +0000

    Part 3 - Completed

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,6 +1,30 @@
 // We use Read.js here and its imported in App.js
-const Create = () => {
-    return <h3>Hello from create component</h3>;
-  };
+import { useState } from "react";
+
+function Create() {
+  const [title, setTitle] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title);
+  }
+
+  return (
+    <div>
+      <h2>This is my Create Component.</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Add Movie Title: </label>
+          <input type="text"
+            className="form-control"
+            value={title}
+            onChange={(e) => { setTitle(e.target.value) }}
+          />
+        </div>
+        <input type="submit" value="Add Movie" />
+      </form>
+    </div>
+  );
+}
   // This gets called in app.js to use header function
   export default Create;
